f053ed5238bacfe6dabf6fde5733f627025800b5
tnarvaez006-del <tnarvaez006@gmail.com>
2026-02-05 20:59:24 -0600

    hsfijdsfbiudsbfiusdfbiusbi

--- RPubliX/settings.py
+++ RPubliX/settings.py
@@ -125,9 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = []
-AUTH_USER_MODEL = "appR.Usuario"
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
