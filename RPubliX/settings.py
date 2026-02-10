2fe077c73023f293cb876547f3d92fc036b3c915
victorpavon2908 <victorpavon2908@gmail.com>
2026-02-10 15:26:36 -0600

    juhfisfsuhoio

--- RPubliX/settings.py
+++ RPubliX/settings.py
@@ -25,12 +25,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "republix.online",
+    "www.republix.online",]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://republix.online",
+    "https://www.republix.online",
+]
 
 # Application definition
 
